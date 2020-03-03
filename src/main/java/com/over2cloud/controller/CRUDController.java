79534a806a630e9b8f635dba0959900c9ca8f735
Sandeep <sandeep2107singh@gmail.com>
2020-03-03 21:53:25 +0530

    Done

--- src/main/java/com/over2cloud/controller/CRUDController.java
+++ src/main/java/com/over2cloud/controller/CRUDController.java
@@ -39,7 +39,6 @@ public class CRUDController {
 	public List fetchList() {
 		List list=new ArrayList();
 		try {
-			
 			return service.findAll();
 		}catch(Exception e) {
 			e.printStackTrace();
